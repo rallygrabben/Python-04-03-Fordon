dd8dcf7d3864fc1a02002a6e64347d41144f262c
Linus Modin <limo0223@storsjogymnasiet.se>
2023-04-03 13:01:46 +0200

    personbil.py

---
+++ fordon.py
@@ -0,0 +1,21 @@
+# Detta är en basklass som ska ärvas 
+
+# constructor initieras först 
+class Fordon:   
+    def __init__(self, fabrikat, color):
+        self.fabrikat = fabrikat
+        self.color = color
+
+    # Metoder
+    def set_fabrikat(self, fabrikat):
+        self.fabrikat = fabrikat
+
+
+    def get_fabrikat(self):
+        return self.fabrikat
+
+    def set_color(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
---
+++ personbil.py
@@ -0,0 +1,17 @@
+# klass som ärver fordon
+
+class personbil: 
+    # constructor
+    def __init__(self, fabrikat, color, bagagevolym):
+        self.fabrikat = fabrikat
+        self.color = color
+        self.bagagevolym = bagagevolym
+
+    # Metoder
+    def set_bagagevolym(self, bagagevolym):
+        self.bagagevolym = bagagevolym
+
+    def get_bagagevolym(self):
+        return self.bagagevolym
+
+    
