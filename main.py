3222f8744363fe91072cf295acd3554c460f93ab
Linus Modin <limo0223@storsjogymnasiet.se>
2023-04-03 14:21:01 +0200

    print funktion

--- main.py
+++ main.py
@@ -12,21 +12,31 @@ ferrari_gul = personbil.personbil("Ferrari", "röd", 30)
 
 # Lastbilar
 scania_blue = lastbil.lastbil("Scania", "Blå", 3440)
-volvo_grey = lastbil.lastbil("Scania", "Grå", 5000)
+volvo_grey = lastbil.lastbil("Volvo", "Grå", 5000)
 
 
 # Skapar Lista
 PersonbilArray = [volvo_red, opel_red, ferrari_gul]
 LastbilsArray = [scania_blue, volvo_grey]
 
+def print_fordonslista(fordonslista):
+
+    for ett_fordon in fordonslista:
+        if (isinstance(ett_fordon, lastbil.lastbil)):
+            print(f"\n\t{ett_fordon.fabrikat} \n\t\tFärg: {ett_fordon.color} \n\t\tFlak Volym: {ett_fordon.flakvolym} Ton")
+
+        elif (isinstance(ett_fordon, personbil.personbil)):
+            print(f"\n\t{ett_fordon.fabrikat} \n\t\tFärg: {ett_fordon.color} \n\t\tBagage Volym: {ett_fordon.bagagevolym} L")
+
 
 while looping == True:
     
+    print_fordonslista(LastbilsArray)
 
+    go = input("\n Vill du lista fordon igen? (j/n): ")
 
-    go = input(f"\n Vill du lista fordon igen? (j/n): ")
-    print(\n)
-    if go == ("n"):
+   
+
+    if (go == "n"):
         break
 
-print(volvo_red.color)
